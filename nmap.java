2604ad3dddbbbe4485b32a692442c7eb255806e3
seo-yu-jeong <rkdehddnjsqls96@gmail.com>
2020-01-07 20:41:35 +0900

    nmap project

---
+++ nmap.java
@@ -0,0 +1,24 @@
+
+public class nmap {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+	}
+	public static int solution(int[][] matrix, int N, int L) {
+		int ret = 0;
+		
+		for(int i = 0; i < N; i++) { //두번째 칸부터 살펴보겠다,
+			//직전땅의 높이
+			//직전땅이 얼마나 연속이 됐는지
+			
+			//같을때는 
+			
+			//높이 차이가 1보다 커질때  할필요가 없음, false,break걸어줌
+			//직전땅보다 1커졌을때 전에 연속된땅수를 확인.
+			//땅이 낮아졌을때 j를 더욱 진전시켜서 ㅣL칸까지 확인해봐야한다.(true/false)
+			//L값만큼갔으면 BREAK를 걸어주면됨.
+			
+		}
+	}
+}
